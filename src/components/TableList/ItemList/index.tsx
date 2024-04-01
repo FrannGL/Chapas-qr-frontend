b928e43d2c74ee91a9b318aa8c0ed19280f58bee
Franco Galluccio <francoivangalluccio@gmail.com>
2024-03-31 22:01:31 -0300

    ADD QR donwload

--- src/components/TableList/ItemList/index.tsx
+++ src/components/TableList/ItemList/index.tsx
@@ -2,12 +2,13 @@ import styles from "./styles.module.scss";
 import Pencil from "../../../../public/icons/pencil";
 import Delete from "../../../../public/icons/delete";
 import wpLogo from "../../../../public/icons/WA_Logo.svg";
+import QRIcon from "../../../../public/icons/qr";
 import { UserProps } from "@/typescript/users.interface";
 import Image from "next/image";
 import Link from "next/link";
 import QRCode from "react-qr-code";
 import { formatBirthday } from "@/utils/formatBirtday";
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import { useRouter } from "next/navigation";
 
 interface ItemListProps {
@@ -17,8 +18,15 @@ interface ItemListProps {
 }
 
 const ItemList = ({ item, onEdit, onDelete }: ItemListProps) => {
+	const [userId, setUserId] = useState("");
 	const [qrClicked, setQrClicked] = useState(false);
+	const [qrValue, setQrValue] = useState("");
+	const qrRef = useRef<HTMLDivElement>(null);
 	const router = useRouter();
+
+	useEffect(() => {
+		setQrValue(`https://localhost:3000/${item._id}`);
+	}, [item._id]);
 
 	const handleEditClick = () => {
 		onEdit(item._id);
@@ -30,11 +38,42 @@ const ItemList = ({ item, onEdit, onDelete }: ItemListProps) => {
 
 	const handleQrClick = () => {
 		setQrClicked(!qrClicked);
+		setUserId(item._id);
+	};
+
+	const handleDownloadQR = () => {
+		const svg = document.getElementById(userId);
+		if (svg) {
+			const svgData = new XMLSerializer().serializeToString(svg);
+			const canvas = document.createElement("canvas");
+			const ctx = canvas.getContext("2d");
+			if (ctx) {
+				const img = new window.Image();
+				img.onload = () => {
+					canvas.width = img.width;
+					canvas.height = img.height;
+					ctx.drawImage(img, 0, 0);
+					const pngFile = canvas.toDataURL("image/png");
+					const downloadLink = document.createElement("a");
+					downloadLink.download = "qrCode";
+					downloadLink.href = `${pngFile}`;
+					downloadLink.click();
+				};
+				img.src = `data:image/svg+xml;base64,${btoa(svgData)}`;
+			}
+		}
 	};
 
 	return (
 		<div className={styles.body}>
-			<Image src={item.image && item.image} alt={item.name} priority width={80} height={80} className={styles.image} />
+			<Image
+				src={(item.image ?? "") as string}
+				alt={item.name}
+				priority
+				width={80}
+				height={80}
+				className={styles.image}
+			/>
 			<p className={`${styles.data} ${styles.name}`} onClick={() => router.push(`${item._id}`)}>
 				{item.name}
 			</p>
@@ -45,11 +84,19 @@ const ItemList = ({ item, onEdit, onDelete }: ItemListProps) => {
 			<Link href={`https://wa.me/${item.whatsappNumber}`} target='_blank' className={styles.data}>
 				<Image src={wpLogo} alt='Whatsapp Logo' className={styles.wp} /> {item.whatsappNumber}
 			</Link>
-			<QRCode
-				value={`https://localhost:3000/${item._id}`}
-				className={`${styles.qr} ${qrClicked ? styles.clicked : ""}`}
-				onClick={handleQrClick}
-			/>
+			<div ref={qrRef} className={`${qrClicked ? styles.qr_container : ""}`}>
+				<QRCode
+					id={userId}
+					value={qrValue}
+					className={`${styles.qr} ${qrClicked ? styles.clicked : ""}`}
+					onClick={handleQrClick}
+				/>
+				{qrClicked && (
+					<button className={styles.btn_donwload} onClick={handleDownloadQR}>
+						Descargar QR <QRIcon />
+					</button>
+				)}
+			</div>
 			<div className={styles.actions}>
 				<button className={styles.btn} onClick={handleEditClick}>
 					<Pencil />
